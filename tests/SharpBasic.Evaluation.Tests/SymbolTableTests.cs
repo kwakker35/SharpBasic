8a267f36a5204991e29300465dd160dbd05ed3d5
Chris G <cjgrove@sky.com>
2026-03-10 21:19:38 +0000

    test: add SymbolTable parent scope tests

--- tests/SharpBasic.Evaluation.Tests/SymbolTableTests.cs
+++ tests/SharpBasic.Evaluation.Tests/SymbolTableTests.cs
@@ -1,3 +1,4 @@
+using SharpBasic.Ast;
 using Xunit;
 
 namespace SharpBasic.Evaluation.Tests;
@@ -16,7 +17,7 @@ public class SymbolTableTests()
 
         Assert.NotNull(output);
         var slv = Assert.IsType<StringValue>(output);
-        Assert.Equal(testValue,slv);
+        Assert.Equal(testValue, slv);
     }
 
     [Fact]
@@ -33,7 +34,7 @@ public class SymbolTableTests()
 
         Assert.NotNull(output);
         var iv = Assert.IsType<IntValue>(output);
-        Assert.Equal(testValue2,iv);
+        Assert.Equal(testValue2, iv);
     }
 
     [Fact]
@@ -44,5 +45,54 @@ public class SymbolTableTests()
         var output = table.Get("Name");
 
         Assert.Null(output);
+    }
+
+    [Fact]
+    public void Variable_In_Outer_Scope_Readable_From_Inner_Scope()
+    {
+        var parent = new SymbolTable();
+        var child = new SymbolTable(parent);
+
+        parent.Set("x", new StringValue("Hello, World!"));
+
+        var value = child.Get("x");
+
+        Assert.NotNull(value);
+        var retVal = Assert.IsType<StringValue>(value);
+        Assert.Equal("Hello, World!", retVal.V);
+    }
+
+    [Fact]
+    public void Variable_In_Inner_Scope_Not_Visible_From_Outer_Scope()
+    {
+        var parent = new SymbolTable();
+        var child = new SymbolTable(parent);
+
+        child.Set("x", new StringValue("Hello, World!"));
+
+        var value = parent.Get("x");
+
+        Assert.Null(value);
+    }
+
+    [Fact]
+    public void Variable_In_Inner_Scope_Shadows_Varible_From_Outer_Scope()
+    {
+        var parent = new SymbolTable();
+        var child = new SymbolTable(parent);
+        parent.Set("x", new StringValue("Hello, World!"));
+        child.Set("x", new StringValue("Goodbye"));
+
+        var pv = parent.Get("x");
+        var cv = child.Get("x");
+
+        Assert.NotNull(pv);
+        var retValP = Assert.IsType<StringValue>(pv);
+        Assert.Equal("Hello, World!", retValP.V);
+
+        Assert.NotNull(cv);
+        var retValC = Assert.IsType<StringValue>(cv);
+        Assert.Equal("Goodbye", retValC.V);
+
     }
 }
